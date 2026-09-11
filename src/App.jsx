78ba060146d6cc83a97f671402d500096adc2ca2
mamadoubaillobahdala99-code <mamadoubaillobahdala99@gmail.com>
2026-09-11 12:25:49 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { CSS } from "./styles";
 import { AuthScreen } from "./features/auth/AuthScreen";
 import { Shell } from "./Shell";import "./features/question-engine/question-engine.css";
 import "./features/question-engine/multiple-choice.css";
+import "./features/question-engine/bulk-paste.css";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
